dd8b991e019957dfc26b22b40a77e6cde6bc3481
JosueDAH012 <jucalova101@gmail.com>
2021-03-25 16:01:00 -0600

    update backend
    Update backend

---
+++ Controllers/ConsultaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using V_VuelosServiciosWeb.Models;
+
+namespace V_VuelosServiciosWeb.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        private readonly ILogger<ConsultaController> _logger;
+
+        public ConsultaController(ILogger<ConsultaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
